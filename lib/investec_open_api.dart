d7a0ea9b6641ea60b04baaf40623e908002bd344
remelehane <reme.lehane@gmail.com>
2021-02-14 07:24:27 +0200

    Initial commit

---
+++ lib/investec_open_api.dart
@@ -0,0 +1,13 @@
+library investec_open_api;
+
+class InvestecOpenAPI {
+  final String clientId;
+  final String secret;
+
+  InvestecOpenAPI({
+    required this.clientId,
+    required this.secret,
+  });
+
+
+}
---
+++ test/investec_open_api_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+import 'package:investec_open_api/investec_open_api.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final api = InvestecOpenAPI(
+      clientId: '',
+      secret: '',
+    );
+  });
+}
