45eb67416ef5eb2f1a08dac3b09487059e61f3d3
remelehane <reme.lehane@gmail.com>
2021-02-20 04:23:01 +0200

    none(app): added example

---
+++ example/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'package:investec_open_api/investec_open_api.dart';
+
+void main() async {
+  final httpClient = http.Client();
+
+  final api = InvestecOpenAPI(
+    clientId: 'API KEY',
+    secret: 'SECRET',
+    httpClient: httpClient,
+  );
+
+  final accounts = await api.getAccounts();
+
+  // This would be all the available accounts.
+  print(accounts.data.accounts); // List<AccountEntity>
+}
