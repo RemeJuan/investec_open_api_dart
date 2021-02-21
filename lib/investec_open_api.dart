93d8b09d254d3453f2edc36621a53ac33b40b680
remelehane <reme.lehane@gmail.com>
2021-02-21 09:10:38 +0200

    feat(account_balance): exposed getAccountBalance from main implementation

--- lib/investec_open_api.dart
+++ lib/investec_open_api.dart
@@ -2,8 +2,10 @@ library investec_open_api;
 
 import 'package:http/http.dart' as http;
 import 'package:investec_open_api/data/sources/access_token_remote_source.dart';
+import 'package:investec_open_api/data/sources/account_balance_remote_source.dart';
 import 'package:investec_open_api/data/sources/accounts_remote_source.dart';
 import 'package:investec_open_api/domain/entities/access_token_entity.dart';
+import 'package:investec_open_api/domain/entities/account_balance_entity.dart';
 import 'package:investec_open_api/domain/entities/accounts_entity.dart';
 
 class InvestecOpenAPI {
@@ -28,5 +30,12 @@ class InvestecOpenAPI {
     final token = await _getAccessToken();
 
     return AccountsRemoteSourceImpl(httpClient, token: token).getAccounts();
+  }
+
+  Future<AccountBalanceEntity> getAccountBalance(String accountId) async {
+    final token = await _getAccessToken();
+
+    return AccountBalanceRemoteSourceImpl(httpClient, token: token)
+        .getBalance(accountId);
   }
 }
